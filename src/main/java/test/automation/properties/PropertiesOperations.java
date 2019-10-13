8f231fb5894dc97b804c453676d056c89a606a29
VIBHOR GOYAL <goyal1992.vibhor@gmail.com>
2019-10-13 14:17:15 +0530

    Added properties reading and writing

---
+++ src/main/java/test/automation/properties/PropertiesOperations.java
@@ -0,0 +1,21 @@
+package test.automation.properties;
+
+import java.io.*;
+import java.util.Properties;
+
+public final class PropertiesOperations {
+
+    public Properties getProperties(String fileName) throws IOException {
+        Properties properties;
+        properties = new Properties();
+        FileInputStream file = new FileInputStream("PropertiesFiles\\Data.properties");
+        properties.load(file);
+        file.close();
+        return properties;
+    }
+
+    public Properties setProperties(Properties properties, String key, String value) throws IOException {
+        properties.setProperty(key, value);
+        return properties;
+    }
+}
